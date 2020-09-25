61836f509206b11f179c59374bcaa66433c61e66
Tanmay Sakhare <68184619+tssakhare45@users.noreply.github.com>
2020-09-25 19:12:54 +0530

    Add files via upload

---
+++ ClassCode/23Sept/ScannerDemo.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class ScannerDemo {
+
+	public static void main (String [] args)   {
+
+		Scanner sc  = new Scanner(System.in);
+
+		int x = sc.nextInt();
+
+		String y = sc.next();
+		
+		float f = sc.nextFloat();
+
+		char h = sc.next().charAt(0);
+
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(f);
+		System.out.println(h);
+
+
+
+	}
+
+
+
+
+}
